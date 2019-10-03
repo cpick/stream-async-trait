34697f8d84abed1732b3136b397877304717b8bb
Chris Pick <chris@chrispick.com>
2019-10-03 19:11:31 -0400

    Fill in example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use async_trait::async_trait;
+use futures::{
+    future::{self, LocalBoxFuture},
+    stream, StreamExt,
+    executor::block_on_stream,
+};
+
+#[async_trait(?Send)]
+pub trait Make {
+    // working
+    fn make<'a, T: Default + 'a>(&self) -> LocalBoxFuture<'a, T> {
+        Box::pin(future::ready(T::default()))
+    }
+
+    // broken
+    // async fn make<'a, T: Default + 'a>(&self) -> T {
+    //     T::default()
+    // }
 }
+
+struct Foo {}
+impl Make for Foo {}
+
+fn main() {
+    let f = Foo {};
+    block_on_stream(stream::empty().map(move |_: ()| f.make::<bool>()));
+}
+
